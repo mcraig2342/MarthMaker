fc51e9d255095e3e9b3a04e9b9674621c90cbc8b
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-10 11:48:55 -0600

    Set up tech area component without props

--- src/Components/TechArea/TechArea.js
+++ src/Components/TechArea/TechArea.js
@@ -1,3 +1,21 @@
 import { React } from 'react';
 import TechCard from '../TechCard/TechCard.js'
 import 'TechArea.css'
+
+
+const TechArea = ({ techs }) => {
+  const techCards = techs.map(tech => {
+  return (
+    <TechCard />
+  )
+})
+
+return (
+  <div className= 'tech-area'>
+    {techCards}
+  </div>
+)
+}
+
+export default TechArea;
+}
