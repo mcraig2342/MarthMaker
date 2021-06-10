9dfbc9dd4070297e0a15cc764030e87b918dc752
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-10 11:56:15 -0600

    Fix import

--- src/Components/TechArea/TechArea.js
+++ src/Components/TechArea/TechArea.js
@@ -1,21 +1,22 @@
 import { React } from 'react';
 import TechCard from '../TechCard/TechCard.js'
-import 'TechArea.css'
+import './TechArea.css'
 
 
-const TechArea = ({ techs }) => {
+const TechArea = ({
+  techs
+}) => {
   const techCards = techs.map(tech => {
-  return (
-    <TechCard />
-  )
-})
+    return ( <
+      TechCard / >
+    )
+  })
 
-return (
-  <div className= 'tech-area'>
-    {techCards}
-  </div>
-)
+  return (
+    <div className='tech-area'>
+    { techCards }
+    </div>
+  )
 }
 
 export default TechArea;
-}
