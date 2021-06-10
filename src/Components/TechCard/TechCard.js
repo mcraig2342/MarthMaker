6c1574987324567f58751052258e475881249634
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-10 13:42:47 -0600

    Create tech card component

--- src/Components/TechCard/TechCard.js
+++ src/Components/TechCard/TechCard.js
@@ -1,3 +1,16 @@
 import React from 'react';
 import './TechCard.css';
-import { Link } from 'react-router-dom'
+
+const TechCard =({ techName, gif, id }) => {
+
+  return (
+    <div className='card' id={id}>
+      <h1>{techName}</h1>
+      <video controls className="video-frame" id="video-skinnycorrupthake" alt="GIF"  playsInline="">
+      <source className='tech-gif' src={gif} type="video/mp4"/>
+      </video>
+    </div>
+  )
+}
+
+export default TechCard;
