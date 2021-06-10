e85d411257e58b1954a965610376bb614a471c4c
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-10 14:23:12 -0600

    Add functionality for video to be played on hover

--- src/Components/TechCard/TechCard.js
+++ src/Components/TechCard/TechCard.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './TechCard.css';
 
+const playMovie = (event) => {
+  event.target.play();
+  console.log('on');
+}
+
 const TechCard =({ techName, gif, id }) => {
 
   return (
     <div className='card' id={id}>
       <h1>{techName}</h1>
-      <video controls className="video-frame" id="video-skinnycorrupthake" alt="GIF"  playsInline="">
+      <video onMouseOver={playMovie} className="video-frame" id="video-skinnycorrupthake" alt="GIF"  playsInline="">
       <source className='tech-gif' src={gif} type="video/mp4"/>
       </video>
     </div>
