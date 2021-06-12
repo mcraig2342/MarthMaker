94fb030bf4ba9b1c544290e6ba15a40d0f077c81
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-12 09:45:34 -0600

    When the button is clicked, the tech is added to the learning list state

--- src/Components/TechCard/TechCard.js
+++ src/Components/TechCard/TechCard.js
@@ -3,17 +3,17 @@ import './TechCard.css';
 
 const playMovie = (event) => {
   event.target.play();
-  console.log('on');
 }
 
-const TechCard =({ techName, gif, id }) => {
+const TechCard =({ addToList, techName, gif, id }) => {
 
   return (
     <div className='card' id={id}>
       <h1>{techName}</h1>
       <video onMouseOver={playMovie} className="video-frame" id="video-skinnycorrupthake" alt="GIF"  playsInline="">
-      <source className='tech-gif' src={gif} type="video/mp4"/>
+        <source className='tech-gif' src={gif} type="video/mp4"/>
       </video>
+      <button id={id} onClick={addToList}>Add to learning list</button>
     </div>
   )
 }
