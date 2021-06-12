97930be11328e6242eb49077f538fb0a3dbf2a01
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-12 10:07:46 -0600

    Add router to view learning list page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import { fetchAllTech } from '../../Utils/api-calls';
 import TechArea from '../TechArea/TechArea.js';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 class App extends Component {
 
@@ -26,10 +26,18 @@ class App extends Component {
   render () {
     return (
     <main className="App">
+    <Link to="/learning_list">
+      <h1>Learning List</h1>
+    </Link>
     <Route exact path ='/'
           render={() => (
             <TechArea addToList={this.addToLearningList} techs={this.state.techs} playMovie={this.playMovie}/>
           )}
+        />
+    <Route exact path ='/learning_list'
+          render={() => (
+            <TechArea techs={this.state.learningList} playMovie={this.playMovie}/>
+         )}
         />
     </main>
   )
@@ -42,7 +50,6 @@ class App extends Component {
   addToLearningList = (event) => {
    const learningList = this.state.learningList
    const techToAdd = this.state.techs.find( tech => tech.id === event.target.id)
-    console.log(parseInt(event.target.id))
      if (!learningList.includes(techToAdd)) {
       this.setState({learningList: [...learningList, techToAdd]})
   }
