9e95a03e7d1b76852eaa314b2a3b3936183d544c
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-13 14:28:14 -0600

    Added logo to nav header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../Utils/logo.png'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
 
   return(
     <nav>
+      <img className='logo' src={logo} alt='Marth maker logo'/>
       <Link to="/learning_list">
         <p>My Learning List</p>
       </Link>
