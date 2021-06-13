49d5d9ac4f54b9aa529a99f1d443f8867e0f07cf
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-13 17:50:31 -0600

    Create home page component

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+
+  return(
+      <div className='home-container'>
+        <h1>Welcome to Marth Maker</h1>
+        <p>
+        Marth Maker is a tech guide that will help you improve
+        your tech-skill and make yourself a better Marth player. The tech
+        page has 31 beginner, intermediate, and advanced tech skills that
+        you should be learning whether you are a Marth player or not.
+        You can add a tech-skill to the learning list so you can keep track
+        of what you have learned so far! To get started head over to the 'TECH'
+        page!
+        </p>
+      </div>
+
+
+  )
+}
+
+export default HomePage;
