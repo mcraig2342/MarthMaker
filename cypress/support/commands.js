969e635e47061cd0def30b2d31604a012ffe3b64
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-14 12:01:33 -0600

    Fix intercept url

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('stub', () => {
   cy.fixture('mock-data.json')
     .then(mockData => {
-      cy.intercept('http://smashlounge.com/api/techs/all', {
+      cy.intercept('http://localhost:3000/api/techs/all', {
         statusCode: 201,
         delay: 100,
         body: mockData
