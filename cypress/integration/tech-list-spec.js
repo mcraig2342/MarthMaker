4f9430ec85cdb27d28092d5cc2aa41178600369b
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-14 12:34:25 -0600

    Add test to check data displayed on cards

--- cypress/integration/tech-list-spec.js
+++ cypress/integration/tech-list-spec.js
@@ -10,4 +10,12 @@ describe('Tech list elements', () => {
       cy.get('[data-cy=tech-card]').should('be.visible')
   });
 
+  it('Should display all required info on tech cards', () => {
+      cy.get('[data-cy=tech-name]').should('be.visible')
+        .get('[data-cy=gif]').should('be.visible')
+        .get('[data-cy=info]').should('be.visible')
+        .get('[data-cy=description]').should('be.visible')
+        .get('[data-cy=list-button]').should('contain', 'Add')
+  });
+
 });
