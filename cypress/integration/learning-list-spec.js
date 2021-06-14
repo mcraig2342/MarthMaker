289d7c3f27feca039da994002c02f3f9fa64312b
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-14 13:29:44 -0600

    Add test to check card info when in list

--- cypress/integration/learning-list-spec.js
+++ cypress/integration/learning-list-spec.js
@@ -10,7 +10,19 @@ describe('Learning list elements and functionality', () => {
       cy.get('[data-cy=list-button]').click()
         .get('[data-cy=list-button]').should('contain', 'Remove')
         .get('[data-cy=learning-link]').click().url().should('eq', 'http://localhost:3000/learning_list')
-        .get('[data-cy=tech-card]').should('be.visible')
+        .get('[data-cy=tech-card]').should('contain', 'Wave Dash')
+  });
+
+  it('Should display all required info on tech cards in learning list', () => {
+    cy.get('[data-cy=list-button]').click()
+      .get('[data-cy=list-button]').should('contain', 'Remove')
+      .get('[data-cy=learning-link]').click().url().should('eq', 'http://localhost:3000/learning_list')
+      .get('[data-cy=tech-card]').should('contain', 'Wave Dash')
+      .get('[data-cy=tech-name]').should('be.visible')
+      .get('[data-cy=gif]').should('be.visible')
+      .get('[data-cy=info]').should('be.visible')
+      .get('[data-cy=description]').should('be.visible')
+      .get('[data-cy=list-button]').should('contain', 'Remove')
   });
 
 });
