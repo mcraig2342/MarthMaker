033c58e692b821f3a242222354113bab7bf0e357
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-14 12:25:53 -0600

    add data-cy for tech cards

--- src/Components/TechCard/TechCard.js
+++ src/Components/TechCard/TechCard.js
@@ -10,7 +10,7 @@ const TechCard =({ inputs, description, inLearningList, removeFromList, addToLis
 
 if(!inLearningList) {
   return (
-    <div className='card' id={id}>
+    <div data-cy='tech-card' className='card' id={id}>
       <h1 className='tech-name'>{techName}</h1>
       <video onMouseOver={playMovie} className="video-frame" id="video-skinnycorrupthake" alt="GIF"  playsInline="">
         <source className='tech-gif' src={gif} type="video/mp4"/>
