ea9ad07da54c6d95f472a4ae970f0c78ce0ce301
mcraig2342 <matthewcookecraig10@gmail.com>
2021-06-14 16:05:58 -0600

    Remove un needed props and added render idem ti render tech cards.

--- src/Components/TechArea/TechArea.js
+++ src/Components/TechArea/TechArea.js
@@ -1,49 +1,20 @@
 import { React } from 'react';
-import TechCard from '../TechCard/TechCard.js';
 import PropTypes from 'prop-types';
 import './TechArea.css';
-let inLearningList;
 
 
-const TechArea = ({ learningList, removeFromList, addToList, techs, playMovie }) => {
+const TechArea = ({ techs, renderItem}) => {
 
-  const displayTech = () => {
-    const learningId = learningList.map(tech => tech.id)
-    return techs.map(tech => {
-        if(learningId.includes(tech.id)) {
-           inLearningList = true;
-        } else {
-           inLearningList = false;
-        }
-        return (
-          < TechCard
-      techName={tech.tech}
-      gif={`https://thumbs.gfycat.com/${tech.gifs[0].url}-mobile.mp4`}
-      description={tech.description}
-      inputs={tech.inputs}
-      id={tech.id}
-      key={tech.id}
-      playMovie={playMovie}
-      addToList={addToList}
-      removeFromList={removeFromList}
-      inLearningList={inLearningList}
-      />
-        )
-    })
-  }
   return (
     <div className='tech-area'>
-    { displayTech() }
+     <div>{techs.map(tech => renderItem(tech))}</div>
     </div>
   )
 }
 
 TechArea.propTypes = {
-  learningList: PropTypes.array.isRequired,
-  removeFromList: PropTypes.func.isRequired,
-  addToList: PropTypes.func,
   techs: PropTypes.array.isRequired,
-  playMovie: PropTypes.func.isRequired,
+  renderItem: PropTypes.func.isRequired
 };
 
 export default TechArea;
